cd4a1e2dc317938b96c63251c76482cb722db59d
James Saryerwinnie <js@jamesls.com>
2015-02-09 21:28:47 -0800

    Modify jp command to read input from stdin
    Consistent with other jmespath implementations.

--- jp.js
+++ jp.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
-jmespath = require('./jmespath')
+jmespath = require('./jmespath');
 
-if (process.argv.length < 3) {
+process.stdin.setEncoding('utf-8');
+
+
+if (process.argv.length < 2) {
     console.log("Must provide a jmespath expression.");
     process.exit(1);
 }
-jmespath.compile(process.argv[2]);
-console.log(JSON.stringify(jmespath.compile(process.argv[2]), null, 2));
-console.log(JSON.stringify(jmespath.search(JSON.parse(process.argv[3]), process.argv[2])));
+var inputJSON = "";
+
+process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+        inputJSON += chunk;
+    }
+});
+
+process.stdin.on('end', function() {
+    var parsedInput = JSON.parse(inputJSON);
+    console.log(JSON.stringify(jmespath.search(parsedInput, process.argv[2])));
+});
